7d7be2ba3e3868b5740161f9e419c30358b7d4bd
Aishu2612 <94443763+Aishu2612@users.noreply.github.com>
2023-04-25 21:27:08 -0700

    Update News.js

--- src/components/News.js
+++ src/components/News.js
@@ -10,16 +10,19 @@ export default class News extends Component {
     }
 
     componentDidMount() {
+        this.interval = setInterval(() => this.setState({ time: Date.now() }), 10000);
+       
         fetch("https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=97a52a31c6d64af6ae3ba0813409cda1").then(resp => {
             return resp.json()
         }).then(data => {
-            this.setState({ news: data.articles })
+            this.setState({ news: data.articles });
+            console.log(this.state.news);
 
         })
-
-
-
     }
+    componentWillUnmount() {
+        clearInterval(this.interval);
+      }
 
 
     render() {
